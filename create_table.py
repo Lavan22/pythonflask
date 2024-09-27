6416a374d378b19528a0bd5b97e9eee3e17eabef
Lavan22 <kit.24.20bec061@gmail.com>
2024-09-27 12:11:24 +0530

    hi

---
+++ create_table.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+print("Connected to database successfully")
+
+conn.execute ('CREATE TABLE students (name TEXT, address TEXT ,city TEXT, zip TEXT)')
+print("Executed Sccessfully")
+
+conn.close()

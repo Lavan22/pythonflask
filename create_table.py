b588035069a914b2250c6c7814ecbcefdec9abbe
Lavan22 <kit.24.20bec061@gmail.com>
2024-09-27 16:56:48 +0530

    cmt

--- create_table.py
+++ create_table.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print("Connected to database successfully")
 
-conn.execute ('CREATE TABLE students (name TEXT, address TEXT ,city TEXT, zip TEXT)')
+conn.execute ('CREATE TABLE students (name TEXT, address TEXT ,class TEXT)')
 print("Executed Sccessfully")
 
 conn.close()
